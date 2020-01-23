e9b12389779ec57695390aeef9d9c0f99a37d6b6
WimITanudjaja <tanudjaja@mac.com>
2020-01-23 17:11:43 -0500

    Initial Commit

---
+++ CIGithubTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CIGithubTest
+//
+//  Created by Wim Tanudjaja on 1/23/20.
+//  Copyright © 2020 Wim Tanudjaja. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
