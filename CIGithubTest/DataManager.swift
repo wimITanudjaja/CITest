92cc1b41b56ea13151fa5824278c0f1c98fe7bfb
WimITanudjaja <tanudjaja@mac.com>
2020-01-23 17:12:48 -0500

    Initial Commit

---
+++ CIGithubTest/DataManager.swift
@@ -0,0 +1,13 @@
+//
+//  DataManager.swift
+//  CIGithubTest
+//
+//  Created by Wim Tanudjaja on 1/23/20.
+//  Copyright © 2020 Wim Tanudjaja. All rights reserved.
+//
+
+import UIKit
+
+class DataManager: NSObject {
+
+}
